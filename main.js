0ee7c0b3dbc63f82a1ff0c96e4edab83d1c03712
matthnoss <davidgrgaca@icloud.com>
2022-12-27 10:55:35 +0100

    Fixed grid size and added custom grid generation

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
 const mainDiv = document.getElementById('mainDiv');
+const button = document.getElementById('gridBtn');
 
-
-for(let i = 0; i < 16; i++){
-    let columnDiv = document.createElement('div');
-    columnDiv.classList.add('mainerDiv');
-    for(let i = 0; i < 16; i++) {
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('divs');
-        columnDiv.appendChild(newDiv);
+function createGrid(gridValue) {
+    let pixelSize = mainDiv.clientWidth / gridValue;
+    let columnHeight = screen.height / gridValue
+    for(let i = 0; i < gridValue; i++){
+        let columnDiv = document.createElement('div');
+        columnDiv.style.height = `${columnHeight}px`;
+        columnDiv.classList.add('mainerDiv');
+        for(let i = 0; i < gridValue; i++) {
+            let newDiv = document.createElement('div');
+            newDiv.classList.add('divs');
+            newDiv.style.width = `${pixelSize}px`;
+            newDiv.style.height = `${columnHeight}px`;
+            columnDiv.appendChild(newDiv);
+        }
+        mainDiv.appendChild(columnDiv);
     }
-    mainDiv.appendChild(columnDiv);
+    document.querySelectorAll('.divs').forEach(item => item.addEventListener('mouseover', changeColor));
 }
-
-document.querySelectorAll('.divs').forEach(item => item.addEventListener('mouseover', changeColor))
-
 
 function changeColor() {
     this.style.backgroundColor = 'red';
 }
+
+button.addEventListener('click', () => {
+    let gridSize = prompt('Enter new grid size');
+    gridSize = parseInt(gridSize);
+    if(gridSize < 1 || gridSize > 100) {
+        alert("Grid size cannot be less than one or over one hundred.");
+    }
+    while (mainDiv.firstChild) {
+        mainDiv.removeChild(mainDiv.lastChild);
+    }
+    createGrid(gridSize);
+})
